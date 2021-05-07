bc30c04d2b0bc9ec03a397099e3087cb71b66d58
Sarolus <cactussius.primus@gmail.com>
2021-05-07 23:23:43 +0200

    Add if real is negative condition

--- 0x00-math_complex/0-display.c
+++ 0x00-math_complex/0-display.c
@@ -6,9 +6,13 @@
  */
 void display_complex_number(complex c)
 {
-	if (c.re != 0)
+	if (c.re > 0)
 	{
 		printf("%.f", c.re);
+	}
+	else if (c.re < 0)
+	{
+		printf("-%.f", c.re)
 	}
 	if (c.re > 0 && c.im > 0)
 	{
