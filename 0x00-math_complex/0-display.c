10160d7abc1390a3f46a155be8b2c49b9c38d864
Sarolus <cactussius.primus@gmail.com>
2021-05-07 23:27:17 +0200

    Remove negative condition

--- 0x00-math_complex/0-display.c
+++ 0x00-math_complex/0-display.c
@@ -6,21 +6,13 @@
  */
 void display_complex_number(complex c)
 {
-	if (c.re > 0)
+	if (c.re != 0)
 	{
 		printf("%.f", c.re);
-	}
-	else if (c.re < 0)
-	{
-		printf("-%.f", c.re)
 	}
 	if (c.re > 0 && c.im > 0)
 	{
 		printf(" + ");
-	}
-	else if (c.re < 0 && c.im < 0)
-	{
-		printf(" - ");
 	}
 	if (c.im != 0)
 	{
